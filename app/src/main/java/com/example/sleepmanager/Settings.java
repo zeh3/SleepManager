72f2e7e990391c07019d4fcbb19a8461cd2de922
zeh3 <zeh3@illinois.edu>
2019-10-20 16:24:52 -0500

    made ui better

--- app/src/main/java/com/example/sleepmanager/Settings.java
+++ app/src/main/java/com/example/sleepmanager/Settings.java
@@ -79,14 +79,14 @@ public class Settings extends AppCompatActivity {
 
 
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }
